d471b4c61ac86ebbdfb89d77aa5b13e3ccf220cd
murat <muratymn.72@gmail.com>
2016-11-25 15:58:53 +0300

    Thread Splash deneme3

--- app/src/main/java/com/yamankod/threadsplash/MainActivity.java
+++ app/src/main/java/com/yamankod/threadsplash/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //deneme2
+    //deneme2ksdjfasjmjhjj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
